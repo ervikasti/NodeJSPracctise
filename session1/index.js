6fabdfc0d3b4244e182de365d36443e7fd67ba13
vikas tiwari <ervikati@gmail.com>
2025-09-03 13:49:19 +0530

    implementation of Request method

--- session1/index.js
+++ session1/index.js
@@ -8,15 +8,28 @@ const PORT = 8081;
 const server = http.createServer((req,res)=>{
     // will look at how to add path (/)
     const path = req.url;
+    //Will look at different request type
+    const method = req.method;
     if(path === "/"){
-        res.writeHead(200,{'content-Type': 'text/html'});
-        res.write('<h1> Hello you are on / path</h1>');
-        res.end();
+        if(method !== 'GET'){
+            res.writeHead(405,{'Content-Type':'text/html'});
+            res.write('<h1> 405: Method Not Allowed </h1>');
+            res.end();
+        }else{
+            res.writeHead(200,{'content-Type': 'text/html'});
+            res.write('<h1> Hello you are on / path</h1>');
+            res.end();}
     }else if(path==='/sell'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(`Thanks for reaching out `);
-        res.write('<h2>You Order on the way</h2>');
-        res.end();
+        if(method !== 'POST'){
+            res.writeHead(405,{'Content-Type':'text/html'});
+            res.write('<h1> 405: Method Not Allowed </h1>');
+            res.end();
+        }else{
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(`Thanks for reaching out `);
+            res.write('<h2>You Order on the way</h2>');
+            res.end();
+        }
     }
 
 
